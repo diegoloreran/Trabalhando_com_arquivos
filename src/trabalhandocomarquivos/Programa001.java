1f26f9920a7c51973125f75e6411fc83eb6bc3d1
Diego Loreran <diegoloreran@gmail.com>
2023-02-07 10:16:26 -0300

    Lendo arquivo texto com classes File e Scanner

---
+++ src/trabalhandocomarquivos/Programa001.java
@@ -0,0 +1,30 @@
+package trabalhandocomarquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Programa001 {
+	
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\diego\\eclipse-workspace\\trabalhandocomarquivos\\inicial.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+
+}
